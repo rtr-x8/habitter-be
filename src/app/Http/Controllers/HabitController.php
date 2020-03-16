1f30ae650a8ec1f5aca6f8a102485eaf6803d014
rtr-x8 <ryu.2.24@hotmail.co.jp>
2020-03-16 14:46:31 +0900

    aaaa

---
+++ src/app/Http/Controllers/HabitController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Habit;
+use Illuminate\Http\Request;
+
+class HabitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Habit $habit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Habit $habit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Habit $habit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Habit $habit)
+    {
+        //
+    }
+}
